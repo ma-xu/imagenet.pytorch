e9fbae6c714844da2cf384dcea898e0f850a742c
13952522076 <melody@dynamic-10-125-217-219.dynamic.unt.edu>
2019-04-30 10:35:38 -0500

    batch size

--- imagenet_SPPSEResNet50.sh
+++ imagenet_SPPSEResNet50.sh
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.0.1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/imagenet.pytorch/main.py -a spp_se_resnet50 --dist-url 'tcp://127.0.0.1:9995' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0  /home/xm0036/Datasets/ImageNet
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/imagenet.pytorch/main.py -a spp_se_resnet50 -b 256 --dist-url 'tcp://127.0.0.1:9995' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0  /home/xm0036/Datasets/ImageNet
